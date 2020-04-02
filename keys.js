ab8d008594f31da4c8ffa2fe7f90173871f25899
Travis Sceroler <Jsceroler@gmail.com>
2020-04-02 11:44:06 -0500

    Sync with desktop and laptop files

--- keys.js
+++ keys.js
@@ -1,6 +1,9 @@
+var keys = require("./keys.js");
 console.log('this is  loaded');
 
 exports.spotify = {
     id: ProcessingInstruction.env.SPOTIFY_ID,
     secret: ProcessingInstruction.env.SPOTIFY_SECRET
 };
+
+var spotify = new spotify(keys.spotify);
--- liri.js
+++ liri.js
@@ -1,1 +1,12 @@
 require("dotenv").config();
+require("dotenv").config();
+var keys = require("./keys")
+var request = require("request")
+var Spotify = require("node-spotify-api")
+var dateFormat = require("dateFormat")
+var fs = require("fs")
+
+//consert-this function for finding bands in town
+var concertThis = function(artist){
+    
+}
